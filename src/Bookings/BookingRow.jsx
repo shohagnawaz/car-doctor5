0d28044b9afda23abe77befaaf820375a0b6bc33
shohagnawaz <snawazdigital@gmail.com>
2024-10-18 16:59:02 +0600

    end

--- src/Bookings/BookingRow.jsx
+++ src/Bookings/BookingRow.jsx
@@ -1,17 +1,21 @@
-
-const BookingRow = ({ booking }) => {
-  const { customerName, email, date, service, price, img } = booking;
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+  const { _id, date, service, price, img, status } = booking;
+  
   return (
     <tr>
       <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
+        <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
       </th>
       <td>
         <div className="avatar">
           <div className="rounded h-24 w-24">
-            { img && <img src={img} alt="Avatar Tailwind CSS Component" /> }
+            {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
           </div>
         </div>
       </td>
@@ -19,7 +23,11 @@ const BookingRow = ({ booking }) => {
       <td>{date}</td>
       <td>${price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        {
+          status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> :
+          <button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">
+            Please Confirm</button>
+        }
       </th>
     </tr>
   );
--- src/Bookings/Bookings.jsx
+++ src/Bookings/Bookings.jsx
@@ -11,6 +11,44 @@ const Bookings = () => {
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, []);
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Deleted Successfully");
+            const remaining = bookings.filter(booking => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+  const handleBookingConfirm = id => {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "PATCH",
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({status: 'confirm'})
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if(data.modifiedCount > 0) {
+          // update state
+          const remaining = bookings.filter(booking => booking !== id);
+          const updated = bookings.find(booking => booking !== id);
+          updated.status = 'confirm';
+          const newBookings = [updated, ...remaining];
+          setBookings(newBookings);
+        }
+      })
+  }
   return (
     <div>
       <h2 className="text-5xl text-center">Your Bookings: {bookings.length}</h2>
@@ -32,12 +70,14 @@ const Bookings = () => {
             </tr>
           </thead>
           <tbody>
-            {
-                bookings.map(booking => <BookingRow
-                    key={booking._id}
-                    booking={booking}
-                ></BookingRow>)
-            }            
+            {bookings.map((booking) => (
+              <BookingRow 
+                key={booking._id} 
+                booking={booking}
+                handleDelete={handleDelete}
+                handleBookingConfirm={handleBookingConfirm}
+              ></BookingRow>
+            ))}
           </tbody>
         </table>
       </div>
